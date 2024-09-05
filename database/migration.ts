82c2fd110b5cfad35af498e4681d3cea0e94d794
umair <muhammadumair2511@gmail.com>
2024-09-05 17:39:16 +0500

    fix(app): added error handling

--- database/migration.ts
+++ database/migration.ts
@@ -3,10 +3,17 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-function main() {
+async function main() {
   const migrationClient = postgres(validEnvVariables.POSTGRES_URL, { max: 1 });
-  migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
-  migrationClient.CLOSE;
+  try {
+    console.log("running migrations...");
+    await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
+    console.log("migration successfull!");
+  } catch (error) {
+    console.error("migration not successfull!", error);
+  } finally {
+    await migrationClient.end();
+  }
 }
 
 main();
