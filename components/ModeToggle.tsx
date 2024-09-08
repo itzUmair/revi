018a82d6dcb231d08f5202cfbb22384cdd476470
umair <muhammadumair2511@gmail.com>
2024-09-08 18:42:54 +0500

    feat(app): added navbar

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -3,32 +3,42 @@
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
-function getButtonContent(theme?: string) {
+function getButtonContent(theme?: string, variant?: string) {
   if (theme === "dark") {
     return (
       <>
-        <MoonIcon />
-        &nbsp;Dark Mode
+        {variant === "minimal" && <MoonIcon width={20} height={20} />}
+        {variant !== "minimal" && (
+          <>
+            <MoonIcon />
+            &nbsp;{variant !== "minimal" && "Dark Mode"}
+          </>
+        )}
       </>
     );
   } else {
     return (
       <>
-        <SunIcon />
-        &nbsp;Light Mode
+        {variant === "minimal" && <SunIcon width={20} height={20} />}
+        {variant !== "minimal" && (
+          <>
+            <SunIcon />
+            &nbsp;Light Mode
+          </>
+        )}
       </>
     );
   }
 }
 
-function ModeToggle() {
+function ModeToggle({ variant }: { variant?: "minimal" | "normal" }) {
   const { theme, setTheme } = useTheme();
   return (
     <button
-      className="flex items-center"
+      className={`flex items-center ${variant === "minimal" && "mr-2"}`}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {getButtonContent(theme)}
+      {getButtonContent(theme, variant)}
     </button>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,6 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Button } from "./ui/button";
 
 import {
   DropdownMenu,
@@ -14,29 +13,149 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ExitIcon, GearIcon, PersonIcon } from "@radix-ui/react-icons";
+import {
+  BackpackIcon,
+  ExitIcon,
+  GearIcon,
+  HamburgerMenuIcon,
+  MagnifyingGlassIcon,
+  PersonIcon,
+  ReaderIcon,
+} from "@radix-ui/react-icons";
 import { Separator } from "./ui/separator";
 import ModeToggle from "./ModeToggle";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+  const [currentWidth, setCurrentWidth] = useState<number>(1024);
   const pathname = usePathname();
-  const { data, status } = useSession();
+  const { status } = useSession();
 
   const isOnAuthPage = pathname.includes("/auth");
 
+  useEffect(() => {
+    function handleResize() {
+      setCurrentWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  function MobileNavbar() {
+    return (
+      <nav className="flex justify-between items-center px-4 bg-zinc-200/50 dark:bg-slate-950/50 backdrop-blur-sm py-1 fixed w-full top-0">
+        <Link href={"/"} className="flex font-bold items-center">
+          <Image src="/revi-logo.png" alt="revi logo" width={60} height={60} />
+          Revi
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <HamburgerMenuIcon width={25} height={25} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-zinc-200/50 dark:bg-slate-950/50 backdrop-blur-sm py-1">
+            <DropdownMenuLabel>Menu</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={"settings"} className="flex items-center">
+                <MagnifyingGlassIcon />
+                &nbsp;Search
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"settings"} className="flex items-center">
+                <BackpackIcon />
+                &nbsp;Register
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"settings"} className="flex items-center">
+                <ReaderIcon />
+                &nbsp;About us
+              </Link>
+            </DropdownMenuItem>
+            {status === "authenticated" && (
+              <DropdownMenuItem>
+                <Link href={"settings"} className="flex items-center">
+                  <GearIcon />
+                  &nbsp;Settings
+                </Link>
+              </DropdownMenuItem>
+            )}
+            <DropdownMenuItem>
+              <ModeToggle />
+            </DropdownMenuItem>
+            {!isOnAuthPage && status !== "authenticated" && (
+              <>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/auth/login"}
+                    className="border-2 border-brand-color px-2 py-1 rounded-md w-full text-center"
+                  >
+                    Login
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link
+                    href={"/auth/signup"}
+                    className="border-2 border-brand-color bg-brand-color text-white px-2 py-1 rounded-md w-full text-center"
+                  >
+                    Signup
+                  </Link>
+                </DropdownMenuItem>
+              </>
+            )}
+
+            <Separator />
+            {status === "authenticated" && (
+              <DropdownMenuItem>
+                <button className="flex items-center" onClick={() => signOut()}>
+                  <ExitIcon className="rotate-180" />
+                  &nbsp;Log out
+                </button>
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+    );
+  }
+
+  if (currentWidth < 768) {
+    return <MobileNavbar />;
+  }
   return (
-    <nav className="flex justify-between items-center px-4 md:px-32">
+    <nav className="flex justify-between items-center px-4 md:px-16 lg:px-32 bg-zinc-200/50 dark:bg-slate-950/50 backdrop-blur-sm py-1 w-full fixed top-0">
       <Link href={"/"} className="flex font-bold items-center">
         <Image src="/revi-logo.png" alt="revi logo" width={60} height={60} />
         Revi
       </Link>
-      <ul className="flex items-center gap-x-2">
-        <li>Search</li>
-        <li>Register</li>
-        <li>About us</li>
+      <ul className="flex items-center gap-x-3">
+        <li>
+          <Link href={"/search"} className="hover:underline">
+            Search
+          </Link>
+        </li>
+        <li>
+          <Link href={"/register"} className="hover:underline">
+            Register
+          </Link>
+        </li>
+        <li>
+          <Link href={"/about-us"} className="hover:underline">
+            About us
+          </Link>
+        </li>
       </ul>
+
       {!isOnAuthPage && status !== "authenticated" && (
         <ul className="flex items-center gap-x-2">
+          <li>
+            <ModeToggle variant="minimal" />
+          </li>
           <li>
             <Link
               href={"/auth/login"}
