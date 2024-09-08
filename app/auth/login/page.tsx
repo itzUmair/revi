ec5a5b1c37f5540a982707b163a9e49e4cefd4f3
umair <muhammadumair2511@gmail.com>
2024-09-08 16:19:19 +0500

    feat(app): added logo to auth pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "@/lib/serverSession";
 import { LoginForm } from "./LoginPage";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default async function SignInPage() {
   const session = await getSession();
@@ -9,5 +10,16 @@ export default async function SignInPage() {
     redirect("/");
   }
 
-  return <LoginForm />;
+  return (
+    <main>
+      <Image
+        src="/revi-logo.png"
+        alt="revi logo"
+        width={150}
+        height={150}
+        className="mx-auto"
+      />
+      <LoginForm />;
+    </main>
+  );
 }
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,6 +1,7 @@
 import { getSession } from "@/lib/serverSession";
 import { redirect } from "next/navigation";
 import { SignupForm } from "./SignupPage";
+import Image from "next/image";
 
 export default async function page() {
   const session = await getSession();
@@ -9,5 +10,16 @@ export default async function page() {
     redirect("/");
   }
 
-  return <SignupForm />;
+  return (
+    <main>
+      <Image
+        src="/revi-logo.png"
+        alt="revi logo"
+        width={150}
+        height={150}
+        className="mx-auto"
+      />
+      <SignupForm />
+    </main>
+  );
 }
