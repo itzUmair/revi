9689665a03be7288b1f13b2ea34f94efb56e93d7
umair <muhammadumair2511@gmail.com>
2024-09-11 18:39:11 +0500

    feat(app): added authorization throughout admin dashboard

--- app/(dashboard)/admin/dashboard/reports/page.tsx
+++ app/(dashboard)/admin/dashboard/reports/page.tsx
@@ -1,4 +1,11 @@
+import { getSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 async function page() {
+  const session = await getSession();
+
+  if (!session || session.user.user_type !== 1) redirect("/");
+
   return <div>page</div>;
 }
 
